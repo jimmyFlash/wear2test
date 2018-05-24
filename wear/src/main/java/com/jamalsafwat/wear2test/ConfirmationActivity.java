4c4e72b33b90d56b97e7affed4a9da2d1592089f
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2018-05-24 13:59:48 +0200

    - substitute WatchStub deprecated class with watch face specific design

--- wear/src/main/java/com/jamalsafwat/wear2test/ConfirmationActivity.java
+++ wear/src/main/java/com/jamalsafwat/wear2test/ConfirmationActivity.java
@@ -13,12 +13,8 @@ public class ConfirmationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        mTextView = (TextView) findViewById(R.id.text);
+
     }
 }
